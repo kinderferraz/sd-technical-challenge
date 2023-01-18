412792410d67d53289e96134d95432354f2cc205
kinderferraz <ferraz.alkindar@gmail.com>
2023-01-18 09:58:45 -0300

    CHL Model: draft repositories

--- src/main/java/com/sd/challenge/booth/data/repositories/PollRepository.java
+++ src/main/java/com/sd/challenge/booth/data/repositories/PollRepository.java
@@ -1,7 +1,13 @@
 package com.sd.challenge.booth.data.repositories;
 
 import com.sd.challenge.booth.data.entities.Poll;
+import com.sd.challenge.booth.data.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
 
-public interface PollRepository extends CrudRepository<Poll, Long> { }
+public interface PollRepository extends CrudRepository<Poll, Long> {
+
+    Set<Poll> findAllByOwner(User user);
+
+}
