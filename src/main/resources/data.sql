6b33a87236a0333efe879aa7aa380e2cebf39c10
kinderferraz <ferraz.alkindar@gmail.com>
2023-01-18 00:55:26 -0300

    CHL Model: add users do in-memory database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- estes usuarios são colocados apenas para iniciar a base,
+-- pretendo adicionar um endpoint ao profile de desenvolvimento para isso, visando
+-- automatizar os testes de carga
+insert into USERS(dsc_cpf, dsc_name) values ("123.456.789.00", "Kinder Rodrigues");
+insert into USERS(dsc_cpf, dsc_name) values ("123.456.987.00", "Ana Paula Padrão");
+insert into USERS(dsc_cpf, dsc_name) values ("123.654.789.00", "Paolla Carosella");
+insert into USERS(dsc_cpf, dsc_name) values ("123.147.789.00", "Erick Jacquin");
+insert into USERS(dsc_cpf, dsc_name) values ("123.658.789.00", "Henrique Fogaça");
+insert into USERS(dsc_cpf, dsc_name) values ("565.456.789.00", "Helena Rizzo");
+insert into USERS(dsc_cpf, dsc_name) values ("988.456.789.00", "Renata Lo Prete");
+insert into USERS(dsc_cpf, dsc_name) values ("055.456.789.00", "William Bonner");
+insert into USERS(dsc_cpf, dsc_name) values ("125.456.789.00", "Renata Vasconcelos");
+insert into USERS(dsc_cpf, dsc_name) values ("459.456.789.00", "Fatima Bernardes");
