80c9772f54385ca1002754040c5e2aef6e4c47fa
kinderferraz <ferraz.alkindar@gmail.com>
2023-01-19 10:35:10 -0300

    CHL UI: prepare acccept form for open poll

--- src/main/java/com/sd/challenge/booth/resources/Screens/AcceptedForm.java
+++ src/main/java/com/sd/challenge/booth/resources/Screens/AcceptedForm.java
@@ -1,18 +1,26 @@
 package com.sd.challenge.booth.resources.Screens;
 
+import com.sd.challenge.booth.resources.exception.PollException;
 import com.sd.challenge.booth.resources.widgets.Element;
 import com.sd.challenge.booth.resources.widgets.Form;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AcceptedForm {
 
     public static Form get(String baseUrl, Map<String, String> data) {
-
         String voteTitle = data.get("accept") != null ? "Obrigado pelo seu voto" : null;
         String newPollTitle = data.get("newPoll") != null ? "Iniciativa recebida" : null;
+        String openPollTitle = data.get("pollOpen") != null ? "Votação aberta" : null;
 
-        Map<String, String> replacementData = Map.of("userId", data.get("userId"));
+        String userId = data.get("userId");
+        String finalTitle = chooseTitle(voteTitle, newPollTitle, openPollTitle, userId);
+
+        Map<String, String> replacementData = Map.of("userId", userId);
 
         Element buttonOk = Element.builder()
                 .texto("Voltar ao início")
@@ -21,9 +29,27 @@ public class AcceptedForm {
                 .build();
 
         return Form.builder()
-                .titulo(voteTitle == null ? newPollTitle : voteTitle)
+                .titulo(finalTitle)
                 .botaoOk(buttonOk)
                 .build();
+
+    }
+
+    private static String chooseTitle(
+            String voteTitle, String newPollTitle,
+            String openPollTitle, String userId
+    ) {
+        List<String> list = Stream.of(voteTitle, newPollTitle, openPollTitle)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        if (list.size() != 1)
+            throw PollException.builder()
+                    .message("too many options passed to accept form")
+                    .data(Map.of("userId", userId))
+                    .build();
+
+        return list.get(0);
 
     }
 
