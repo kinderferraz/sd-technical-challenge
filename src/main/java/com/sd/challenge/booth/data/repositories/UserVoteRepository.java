63f1be14bf3d4fdea75f0d6e5b6e115b86a25ed8
kinderferraz <ferraz.alkindar@gmail.com>
2023-01-19 08:04:45 -0300

    CHL FIX: typo in UserVoteRepository
    pendente:
    - implementar tratamento de erro
    - conferir propagacão de dados entre paginas

--- src/main/java/com/sd/challenge/booth/data/repositories/UserVoteRepository.java
+++ src/main/java/com/sd/challenge/booth/data/repositories/UserVoteRepository.java
@@ -4,10 +4,9 @@ import com.sd.challenge.booth.data.entities.UserVote;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserVoteRepostory extends CrudRepository<UserVote, Long> {
+public interface UserVoteRepository extends CrudRepository<UserVote, Long> {
 
     @Query("select uv from UserVote uv where uv.poll.id = (:pollId) and uv.voter.id = (:voterId) ")
     UserVote findUserVoteByPollAndVoter(Long pollId, Long voterId);
-
 
 }
