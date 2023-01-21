114b595a889552e3c8e78a9fed0768659172eb9d
kinderferraz <ferraz.alkindar@gmail.com>
2023-01-21 09:02:18 -0300

    CHL TEST: implement GetPollGatewayTest

---
+++ src/test/java/com/sd/challenge/booth/ui/poll_gateway/GetPollGatewayTest.java
@@ -0,0 +1,24 @@
+package com.sd.challenge.booth.ui.poll_gateway;
+
+import com.sd.challenge.booth.resources.Screens.PollGatewaySelection;
+import com.sd.challenge.booth.resources.widgets.Selection;
+import com.sd.challenge.booth.ui.UiMvcTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class GetPollGatewayTest extends UiMvcTest {
+
+    @Test
+    void getPollGatewayTest() throws Exception {
+        Map<String, String> data = Map.of("idtCpfInput", "19279699059");
+
+        Selection gateway = PollGatewaySelection.get(baseUrl, Map.of("userId", "1"));
+        performPostRequest("/ui/poll-gateway", data, status().isOk(),
+                content().json(gson.toJson(gateway)));
+    }
+
+}
