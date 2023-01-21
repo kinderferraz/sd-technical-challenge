0f2a85cd5ec562e116da7cec0d18c50dd728d6f9
kinderferraz <ferraz.alkindar@gmail.com>
2023-01-21 10:17:08 -0300

    CHL SRV: refactor exception generation

--- src/main/java/com/sd/challenge/booth/services/PollService.java
+++ src/main/java/com/sd/challenge/booth/services/PollService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -58,19 +59,18 @@ public class PollService {
 
     public void castVote(Map<String, String> data) {
         boolean voteValue = Boolean.parseBoolean(data.get("accept"));
+
         Long pollId = Long.valueOf(data.get("pollId"));
         Long userId = Long.valueOf(data.get("userId"));
 
         Poll poll = getPoll(pollId, "M=castVote", userId);
-        User user = getUser(userId, "M=castVote");
+        if (poll.getOwner().getId().equals(userId))
+            throw makeException("M=castVote user owns poll userId={0} pollId={1}", userId, pollId);
 
+        User user = getUser(userId, "M=castVote");
         if (userVoteRepository.findUserVoteByPollAndVoter(pollId, userId) != null)
-            throw PollException.builder()
-                    .message("user already cast vote on poll" +
-                            " userId=" + userId +
-                            " pollId=" + pollId )
-                    .data(Map.of("userId", userId.toString()))
-                    .build();
+            throw makeException("M=castVote user already cast vote on poll userId={0} pollId={1}",
+                    userId, pollId);
 
         UserVote vote = UserVote.builder()
                 .vote(voteValue)
@@ -104,17 +104,12 @@ public class PollService {
         Poll poll = getPoll(pollId, "M=openPoll", userId);
 
         if(! poll.getOwner().getId().equals(userId))
-            throw PollException.builder()
-                    .message("M=openPoll user does not own poll poll=" + pollId +
-                            " userId=" + userId)
-                    .data(Map.of("userId", String.valueOf(userId)))
-                    .build();
+            throw makeException("M=openPoll user does not own poll userId={0} pollId={1}",
+                    userId, pollId);
 
         if(poll.getOpenedAt() != null)
-            throw PollException.builder()
-                    .message("M=openPoll poll aleady open=" + pollId)
-                    .data(Map.of("userId", String.valueOf(userId)))
-                    .build();
+            throw makeException("M=openPoll poll already open userId={0} pollId={1}",
+                    userId, pollId);
 
         poll.setOpenedAt(LocalDateTime.now());
         poll.setEndsAt(getEndingTime(data));
@@ -131,8 +126,10 @@ public class PollService {
 
     private Poll getPoll(Long pollId, String method, Long userId) {
         return pollRepository.findById(pollId)
-                .orElseThrow(() -> PollException.builder()
-                        .message(method + " poll not found pollId=" + pollId)
+                .orElseThrow(
+                        () -> PollException.builder()
+                        .message(MessageFormat.format("{0} poll not found pollId={1}",
+                                method, pollId))
                         .data(Map.of("userId", String.valueOf(userId)))
                         .build());
     }
@@ -140,9 +137,17 @@ public class PollService {
     private User getUser(Long userId, String method) {
         return userRepository.findById(userId)
                 .orElseThrow(() -> PollException.builder()
-                        .message(method + " user not found userId="+userId)
+                        .message(MessageFormat.format("{0} user not found userId={1}",
+                                method, userId))
                         .data(Map.of("userId", String.valueOf(userId)))
                         .build());
+    }
+
+    private PollException makeException(String message, Long userId, Long pollId){
+        return PollException.builder()
+                .message(MessageFormat.format(message, userId, pollId))
+                .data(Map.of("userId", String.valueOf(userId)))
+                .build();
     }
 
 }
