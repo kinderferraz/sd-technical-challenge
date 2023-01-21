4b3dee96725dc4285c7ac4d3c674f11c1055ba79
kinderferraz <ferraz.alkindar@gmail.com>
2023-01-21 10:42:18 -0300

    CHL TEST: gateway error test on cast vote test

--- src/test/java/com/sd/challenge/booth/api/PostNewVoteTest.java
+++ src/test/java/com/sd/challenge/booth/api/PostNewVoteTest.java
@@ -32,21 +32,23 @@ public class PostNewVoteTest extends MvcTest {
 
     public static Stream<Arguments> getPostNewVoteTestArguments() {
         return Stream.of(
-                Arguments.of("Success Vote Test", "1", "5", "open", true),
-                Arguments.of("Already Voted Test", "1", "4", "open", false),
-                Arguments.of("Vote on Own Poll Test", "1", "2", "own", false)
+                Arguments.of("Success Vote Test", "1", "5", "open", "accept", true),
+                Arguments.of("Already Voted Test", "1", "4", "open", "accept", false),
+                Arguments.of("Vote on Own Poll Test", "1", "2", "own", "accept", false),
+                Arguments.of("Gateway Error Test", "1", "2", "own", "newPoll", false)
+
         );
     }
 
     @ParameterizedTest(name = "{0}")
     @MethodSource("getPostNewVoteTestArguments")
     void postNewVoteTest(
-            String name, String userId, String pollId, String listing, Boolean success
+            String name, String userId, String pollId, String listing, String gateway, Boolean success
     ) throws Exception {
         Map<String, String> postData = Map.of(
                 "userId", userId,
                 "pollId", pollId,
-                "accept", "true",
+                gateway, "true",
                 "listingOf", listing
         );
 
